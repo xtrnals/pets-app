5ff787c3424354a7835b58f5c13ab149f981ce15
Jasfer John <47032082+xtrnals@users.noreply.github.com>
2025-04-30 09:58:27 +0400

    Add files via upload

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = 3000;
 
 // Connect to MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/petsDB', {
+mongoose.connect('mongodb://localhost:27017/', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => console.log('MongoDB connected'))
